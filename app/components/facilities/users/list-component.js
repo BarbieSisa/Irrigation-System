77e1f00f413cbd7e77e48c52f436e3e7f7d37b22
nchakarov98 <nchakarov98@gmail.com>
2024-02-28 19:24:00 +0200

    fix

--- app/components/facilities/users/list-component.js
+++ app/components/facilities/users/list-component.js
@@ -1,17 +1,22 @@
 import InfinityScrollComponent from 'irrigation-system/base-elements/infinity-scroll-component';
 import { tracked } from '@glimmer/tracking';
+import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 export default class UsersListComponent extends InfinityScrollComponent {
   @service('role-type') roleTypes;
+  @service('current-user') currentUser;
 
-  @tracked sortBy = 'party.person.firstName'
+  @tracked sortBy = 'person.firstName'
   @tracked orderBy = 'ASC'
   @tracked includeRestricted = false;
   @tracked includeAllowed = true;
-  @tracked roleTypeId = this.roleTypes.FACILITY_PARTY_ROLE_USER_ASSOC_ID;
+  @tracked excludePartyIds = this.currentUser.get('loggedUser.party.partyId');
+  modelName = 'party';
 
-  modelName = 'facility-party';
-  endPoint = 'facilityParties'
+  @computed('facilityId')
+  get endPoint(){
+    return 'facilities/' + this.facilityId + '/parties';
+  }
 
-  queryParamsList = ['sortBy', 'orderBy', 'searchText', 'facilityId', 'roleTypeId'];
+  queryParamsList = ['sortBy', 'orderBy', 'searchText', 'facilityId','excludePartyIds'];
 }
